21dc8781069ecf68c4f9e4b218f1408b17aa7a71
imsavi <101518237+imsavi@users.noreply.github.com>
2022-03-16 00:54:43 +0100

    Update info.py

--- extensions/info.py
+++ extensions/info.py
@@ -7,28 +7,6 @@ import datetime
 
 plugin = lightbulb.Plugin("random")
 
-@plugin.command
-@lightbulb.command(name="botty",description="give infos about the bot",aliases=["bt","info","if"])
-@lightbulb.implements(lightbulb.PrefixCommand,lightbulb.SlashCommand)
-async def embed(ctx: lightbulb.Context) -> None:
-        aliases=[]
-        aliases=" ".join(ctx.command.aliases)
-        embed=(
-            hikari.Embed(
-            title=f"{str(ctx.bot.get_me()).upper()}",
-            description=f"{plugin.bot.application.owner.username} is my only owner!!!",
-            timestamp=datetime.datetime.now().astimezone(),
-            colour= hikari.Color(0x7f9c85),
-            )
-            .add_field(name=" I was created in".upper(), value=plugin.bot.application.created_at.astimezone().ctime())
-            .add_field(name="You can call me by these commands:".upper(),value=aliases)
-            .set_footer(
-                text=f"Requested by {ctx.member.display_name}",
-                icon=ctx.member.avatar_url
-            )
-            .set_thumbnail("https://media.vogue.fr/photos/5c8a55363d44a0083ccbef54/2:3/w_1600,c_limit/GettyImages-625257378.jpg")
-        )
-        await ctx.respond(embed, reply=True, mentions_reply=True)
 
 @plugin.command
 @lightbulb.command("help","Get a list of available commands ")
@@ -36,7 +14,7 @@ async def embed(ctx: lightbulb.Context) -> None:
 async def help(ctx: lightbulb.Context) -> None:
     movie_commands=["","👉 !𝖒𝖔𝖛𝖎𝖊 𝖘𝖊𝖆𝖗𝖈𝖍 [𝖒𝖔𝖛𝖎𝖊 𝖓𝖆𝖒𝖊]: 𝕲𝖊𝖙 𝖉𝖊𝖙𝖆𝖎𝖑𝖘 𝖆𝖇𝖔𝖚𝖙 𝖙𝖍𝖊 𝖒𝖔𝖛𝖎𝖊.","👉 !𝖒𝖔𝖛𝖎𝖊 𝖜𝖆𝖙𝖈𝖍 [𝖒𝖔𝖛𝖎𝖊 𝖓𝖆𝖒𝖊]: 𝕲𝖊𝖙 𝖆 𝖉𝖎𝖗𝖊𝖈𝖙 𝖘𝖙𝖗𝖊𝖆𝖒𝖎𝖓𝖌 𝖑𝖎𝖓𝖐𝖘 𝖋𝖔𝖗 𝖜𝖆𝖙𝖈𝖍𝖎𝖓𝖌 𝖆𝖓𝖉 𝖉𝖔𝖜𝖓𝖑𝖔𝖆𝖉𝖎𝖓𝖌.","👉 !𝖒𝖔𝖛𝖎𝖊 𝖙𝖗𝖊𝖓𝖉𝖎𝖓𝖌: 𝕲𝖊𝖙 𝖙𝖗𝖊𝖓𝖉𝖎𝖓𝖌 𝖒𝖔𝖛𝖎𝖊𝖘 𝖙𝖍𝖎𝖘 𝖜𝖊𝖊𝖐.","👉 !𝖒𝖔𝖛𝖎𝖊 𝖘𝖎𝖒𝖎𝖑𝖆𝖗 [𝖒𝖔𝖛𝖎𝖊 𝖓𝖆𝖒𝖊]: 𝕲𝖊𝖙 𝖆 𝖑𝖎𝖘𝖙 𝖔𝖋 𝖘𝖎𝖒𝖎𝖑𝖆𝖗 𝖒𝖔𝖛𝖎𝖊𝖘.","👉 !𝖒𝖔𝖛𝖎𝖊 𝖕𝖔𝖕𝖚𝖑𝖆𝖗: 𝕲𝖊𝖙 𝖆 𝖑𝖎𝖘𝖙 𝖔𝖋 𝖙𝖍𝖊 𝖈𝖚𝖗𝖗𝖊𝖓𝖙 𝖕𝖔𝖕𝖚𝖑𝖆𝖗 𝖒𝖔𝖛𝖎𝖊𝖘."]
     serie_commands=["","👉 !𝖘𝖊𝖗𝖎𝖊 𝖘𝖊𝖆𝖗𝖈𝖍 [𝖘𝖊𝖗𝖎𝖊 𝖓𝖆𝖒𝖊]: 𝕲𝖊𝖙 𝖉𝖊𝖙𝖆𝖎𝖑𝖘 𝖆𝖇𝖔𝖚𝖙 𝖙𝖍𝖊 𝕿𝖁 𝕾𝖍𝖔𝖜.","👉 !𝖘𝖊𝖗𝖎𝖊 𝖜𝖆𝖙𝖈𝖍 [𝖘𝖊𝖗𝖎𝖊 𝖓𝖆𝖒𝖊] [𝖘𝖊𝖆𝖘𝖔𝖓 𝖓𝖚𝖒𝖇𝖊𝖗] [𝖊𝖕𝖎𝖘𝖔𝖉𝖊 𝖓𝖚𝖒𝖇𝖊𝖗]: 𝖂𝖆𝖙𝖈𝖍 𝖆 𝖈𝖍𝖔𝖘𝖊𝖓 𝕰𝖕𝖎𝖘𝖔𝖉𝖊 𝖋𝖗𝖔𝖒 𝖞𝖔𝖚𝖗 𝕿𝖁 𝖘𝖍𝖔𝖜.","👉 !𝖘𝖊𝖗𝖎𝖊 𝖙𝖔𝖕_𝖗𝖆𝖙𝖊𝖉: 𝕲𝖊𝖙 𝖆 𝖑𝖎𝖘𝖙 𝖔𝖋 𝖙𝖍𝖊 𝖙𝖔𝖕 𝖗𝖆𝖙𝖊𝖉 𝕿𝖁 𝖘𝖍𝖔𝖜𝖘.","👉 !𝖘𝖊𝖗𝖎𝖊 𝖕𝖔𝖕𝖚𝖑𝖆𝖗: 𝕲𝖊𝖙 𝖆 𝖑𝖎𝖘𝖙 𝖔𝖋 𝖙𝖍𝖊 𝖈𝖚𝖗𝖗𝖊𝖓𝖙 𝖕𝖔𝖕𝖚𝖑𝖆𝖗 𝕿𝖁 𝖘𝖍𝖔𝖜𝖘.","👉 !𝖘𝖊𝖗𝖎𝖊 𝖘𝖎𝖒𝖎𝖑𝖆𝖗 [𝖘𝖊𝖗𝖎𝖊 𝖓𝖆𝖒𝖊]: 𝕲𝖊𝖙 𝖆 𝖑𝖎𝖘𝖙 𝖔𝖋 𝖘𝖎𝖒𝖎𝖑𝖆𝖗 𝕿𝖁 𝖘𝖍𝖔𝖜𝖘." ]
-    others=["👉 !𝖇𝖔𝖙𝖙𝖞: 𝕴𝖙 𝖌𝖎𝖛𝖊𝖘 𝖎𝖓𝖋𝖔𝖘 𝖆𝖇𝖔𝖚𝖙 𝖙𝖍𝖊 𝖇𝖔𝖙.","👉 !𝖕𝖎𝖓𝖌: 𝕷𝖆𝖙𝖊𝖓𝖈𝖞."]
+    others=["👉 !𝖕𝖎𝖓𝖌: 𝕷𝖆𝖙𝖊𝖓𝖈𝖞."]
     others= "\n".join(others)
     movie_commands="\n".join(movie_commands)
     serie_commands="\n".join(serie_commands)
