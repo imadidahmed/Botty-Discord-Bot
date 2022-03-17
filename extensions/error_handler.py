d8969daa28566cffb67258f8aeaddcf3219fc219
imsavi <101518237+imsavi@users.noreply.github.com>
2022-03-17 23:44:34 +0100

    update

--- extensions/error_handler.py
+++ extensions/error_handler.py
@@ -23,11 +23,11 @@ async def on_error(event: lightbulb.CommandErrorEvent) -> None:
         await event.context.respond("This extension is not found or missing.")
     if isinstance(event.context,lightbulb.CommandIsOnCooldown):
         await event.context.respond("this command is in cooldown repeat after 5 sec !!!")
-    if isinstance(event.context,lightbulb.errors.NotOwner):
-        await event.context.respond("only the owner (SneakyPete) can do this type of commands")
+    if isinstance(event.exception,lightbulb.NotOwner):
+        await event.context.respond("sorry but you cannot use this type of commands (owner only)")
+
     
-    
-    
+
     
     raise event.exception
 
